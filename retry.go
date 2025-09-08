79f7bb59cb7a642cb4db2307948de71218edd6e2
Max Collier <collierwm01@gmail.com>
2025-09-08 11:29:24 +1000

    implemented expontential backoff strategy

--- retry.go
+++ retry.go
@@ -8,10 +8,17 @@ func RetryStrategyImmediate(retries int) time.Duration {
 	return 0
 }
 
-func RetryStrategyExponential(retries int) time.Duration {
-	return time.Duration(retries) * 100 * time.Millisecond
+func RetryStrategyLinearBackoff(timeToWait time.Duration) RetryStrategy {
+	return func(retries int) time.Duration {
+		return time.Duration(retries) * timeToWait
+	}
 }
 
-func RetryStrategyLinear(retries int) time.Duration {
-	return time.Duration(retries) * 100 * time.Millisecond
+func RetryStrategyExponentialBackoff(timeToWait time.Duration) RetryStrategy {
+	return func(retries int) time.Duration {
+		if retries == 0 {
+			return 0
+		}
+		return time.Duration(1<<uint(retries-1)) * timeToWait
+	}
 }
